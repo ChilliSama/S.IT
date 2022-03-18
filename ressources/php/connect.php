6d7b7b991a6a595327273b8fb763ec9d5c331e4b
ChilliSama <chilli380killer@gmail.com>
2022-03-18 16:43:28 +0100

    change mysql relation to PDO

--- ressources/php/connect.php
+++ ressources/php/connect.php
@@ -2,12 +2,19 @@
 
 include "debug.php";
 
-$connect = mysqli_connect("","","","");
+$servername = "localhost";
+$dbname = "sit_db";
+$username = "root";
+$password = "";
 
-if(!$connect){
-    die("Connection failed: " . mysqli_connect_error());
-} else {
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     debug_to_console("Connected to sit_db table ✓");
+} catch(PDOException $e) {
+    error_log($e->getMessage());
+    debug_to_console("Connection failed: " . $e->getMessage());
 }
 
 
