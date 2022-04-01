d344db00ffd20bdaccd690a6eec905da71dedbf2
ChilliSama <chilli380killer@gmail.com>
2022-04-01 11:08:16 +0200

    implement home page and footer

--- ressources/php/home.php
+++ ressources/php/home.php
@@ -1,5 +1,10 @@
-<div class="hero d-flex flex-column justify-content-center">
-    <h1>S.IT</h1>
-    <p>Réservation de bureau au sein d'un OpenSpace.</p>
-    <img src="ressources/img/office1.png" alt="">
+<div class="hero">
+    <div class="custom-mt">
+        <h1 class="display-4">S<span>.</span>IT</h1>
+        <p class="lead custom-weight">Réservation de bureau au sein d'un OpenSpace.</p>
+        <a href="../../../register.php" class="btn btn-outline-success">Réserver !</a>
+    </div>
+    <div class="fixed-bottom d-flex justify-content-end">
+        <img class="img-fluid " src="ressources/img/office1.png" alt="">
+    </div>
 </div>
