c7f4a5c9c9f69ca979ba3f30c0b251de2f5f8729
Terence Dufrane <chilli380killer@gmail.com>
2022-04-22 17:32:35 +0200

    update front

--- ressources/php/calendar.php
+++ ressources/php/calendar.php
@@ -25,42 +25,176 @@
     }
 ?>
 
-<div>
-    <form method="post">
-        <div class="d-flex justify-content-center align-items-center"></br></br></br>
-            <?php if ($_SESSION['day_count'] > -15) {
-                printf("<input type='submit' name='previous_day' class='btn bg-transparent' value='<'>");
-                }?>
-            <?php printf("<h5 style='display:inline-block;'>{$day}</h5>"); ?>
-            <?php if ($_SESSION['day_count'] < 15) {
-                printf("<input type='submit' name='next_day' class='btn bg-transparent' value='>'>");
-                }?>
-        </div></br>
+<div class="justify-content-center text-center container-bg">
+    <form method="post"></br>
+            <div class="row">
+                <div class="col">
+                    <?php if ($_SESSION['day_count'] > -15) {
+                        printf("<input type='submit' name='previous_day' class='btn bg-transparent' value='<'>");
+                        }?>
+                    <?php printf("<h5 style='display:inline-block;'>{$day}</h5>"); ?>
+                    <?php if ($_SESSION['day_count'] < 15) {
+                        printf("<input type='submit' name='next_day' class='btn bg-transparent' value='>'>");
+                        }?>
+                </div>
+            </div>
     </form>
-    <div class="container-fluid justify-content-center text-center">
+    <form method="post">
         <div class="row">
             <div class="col">
-                <img class="calendar-img" src="./ressources\img\rect.png" alt="">
-                <img class="calendar-img" src="./ressources\img\rect.png" alt="">
-                <img class="calendar-img" src="./ressources\img\rect.png" alt="">
-                <img class="calendar-img" src="./ressources\img\rect.png" alt="">
-                <img class="calendar-img" src="./ressources\img\rect.png" alt="">
-                <img class="calendar-img" src="./ressources\img\rect.png" alt="">
-                <img class="calendar-img" src="./ressources\img\rect.png" alt="">
-                <img class="calendar-img" src="./ressources\img\rect.png" alt="">
+                <a class="calendar-img rot " href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img rot" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img rot" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\desk1.png" alt="">
             </div>
         </div>
-        <div class="row justify-content-center">
+        <div class="row">
             <div class="col">
-                <img class="calendar-img" src="./ressources\img\rect.png" alt="">
-                <img class="calendar-img" src="./ressources\img\rect.png" alt="">
-                <img class="calendar-img" src="./ressources\img\rect.png" alt="">
-                <img class="calendar-img" src="./ressources\img\rect.png" alt="">
-                <img class="calendar-img" src="./ressources\img\rect.png" alt="">
-                <img class="calendar-img" src="./ressources\img\rect.png" alt="">
-                <img class="calendar-img" src="./ressources\img\rect.png" alt="">
-                <img class="calendar-img" src="./ressources\img\rect.png" alt="">
+                <a class="calendar-img rot" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img rot" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img rot" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\desk1.png" alt="">
             </div>
         </div>
+        <div class="row">
+            <div class="col">
+                <a class="calendar-img rot" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img rot" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img rot" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\desk1.png" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <a class="calendar-img rot-minus-90" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img rot-minus-90" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img rot-minus-90" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img rot-minus-90" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <a class="calendar-img rot-90" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img rot-90" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img rot-90" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img rot-90" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img rot" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img rot" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <a class="calendar-img rot" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\desk1.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+                <a class="calendar-img" href="./ressources\img\void.png" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <br>
+            </div>
+        </div>
+    </form>
+</div>
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
     </div>
+  </div>
 </div>
